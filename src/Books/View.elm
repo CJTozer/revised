532ed734eedb7d9b13c544f71b8d3d2ff2dea71f
Chris Tozer <christopherjtozer@gmail.com>
2016-10-03 20:39:14 +0100

    inline the helper function

--- src/Books/View.elm
+++ src/Books/View.elm
@@ -8,23 +8,26 @@ import Messages exposing (Msg)
 
 bookList : List Book -> Html Msg
 bookList books =
-    div []
-        [ table []
-            [ thead []
-                [ tr []
-                    [ th [] [ text "Title" ]
-                    , th [] [ text "Author" ]
-                    ]
+    let
+        bookRow : Book -> Html Msg
+        bookRow book =
+            tr []
+                [ td []
+                    [ text book.title ]
+                , td []
+                    [ text book.author ]
                 ]
-            , tbody [] (List.map bookRow books)
+    in
+        div []
+            [ table []
+                [ thead []
+                    [ tr []
+                        [ th [] [ text "Title" ]
+                        , th [] [ text "Author" ]
+                        ]
+                    ]
+                , tbody []
+                    (List.map bookRow books)
+                ]
             ]
-        ]
 
-bookRow : Book -> Html Msg
-bookRow book =
-    tr []
-        [ td []
-            [ text book.title ]
-        , td []
-            [ text book.author ]
-        ]
