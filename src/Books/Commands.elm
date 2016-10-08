c5500872491e38fb2c61c9c0629f5fc118a0b7a2
Chris Tozer <chris.tozer@metaswitch.com>
2016-10-08 13:04:51 +0100

    Try HTTPS

--- src/Books/Commands.elm
+++ src/Books/Commands.elm
@@ -18,7 +18,7 @@ fetchAll =
 
 fetchBooksListUrl : String
 fetchBooksListUrl =
-    "http://revised-server.herokuapp.com/v1/books"
+    "https://revised-server.herokuapp.com/v1/books"
 
 
 bookListDecoder : Decode.Decoder (List Book)
