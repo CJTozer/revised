64ebd27d0885217d84336774c2c833f9fbccf0f6
Chris Tozer <chris.tozer@metaswitch.com>
2016-10-10 23:57:44 +0100

    Tidy up file locations

--- js/index.js
+++ js/index.js
@@ -5,15 +5,15 @@ require('materialize-css/dist/css/materialize.css');
 // require('materialize-css/dist/js/materialize.js');
 
 // Extra CSS
-require('./extra.css');
+require('../css/extra.css');
 
 // Extra JS for running materialize code when the DOM changes
-require('./dom_monitor.js');
+require('../js/dom_monitor.js');
 
 // Require index.html so it gets copied to dist
-require('./index.html');
+require('../static/index.html');
 
-var Elm = require('./Main.elm');
+var Elm = require('../src/Main.elm');
 var mountNode = document.getElementById('main');
 
 // The third value on embed are the initial values for incomming ports into Elm
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require("path");
 module.exports = {
   entry: {
     revised: [
-      './src/index.js'
+      './js/index.js'
     ]
   },
 
