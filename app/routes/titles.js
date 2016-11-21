bcb9ee36348a6806f1fe52684ac3fee247493b54
Chris Tozer <chris.tozer@metaswitch.com>
2016-11-21 00:10:42 +0000

    Default model for listings

--- app/routes/titles.js
+++ app/routes/titles.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
+let titles = [{
+  id: 'longitude-dava-sobel',
+  title: 'Longitude',
+  author: 'Dava Sobel',
+}, {
+  id: 'peter-the-great-robert-k-massie',
+  title: 'Peter The Great',
+  author: 'Robert K. Massie',
+}, {
+  id: 'the-two-koreas-don-oberdorfer',
+  title: 'The Two Koreas',
+  author: 'Don Oberdorfer',
+}];
+
 export default Ember.Route.extend({
+  model() {
+    return titles;
+  }
 });
--- tests/acceptance/list-titles-test.js
+++ tests/acceptance/list-titles-test.js
@@ -3,16 +3,16 @@ import moduleForAcceptance from 'revised/tests/helpers/module-for-acceptance';
 
 moduleForAcceptance('Acceptance | list titles');
 
-test('visiting /', function(assert) {
-  visit('/');
+test('visiting /titles', function(assert) {
+  visit('/titles');
 
   andThen(function() {
-    assert.equal(currentURL(), '/');
+    assert.equal(currentURL(), '/titles');
   });
 });
 
 test('should list books.', function (assert) {
-  visit('/');
+  visit('/titles');
   andThen(function () {
     assert.equal(find('.titles-entry').length, 3, 'should see 3 books');
   });
