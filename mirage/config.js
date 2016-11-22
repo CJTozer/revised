bea71bdcbc8993b3a8d1d8234306d60e9ad52c51
Chris Tozer <chris.tozer@metaswitch.com>
2016-11-22 11:49:07 +0000

    Fake data from mirage factory for development

--- mirage/config.js
+++ mirage/config.js
@@ -1,30 +1,7 @@
 export default function() {
-  this.namespace = '/api';
+  this.namespace = 'api';
 
-  this.get('/titles', function() {
-    return {
-      data: [{
-        type: 'titles',
-        id: 'longitude-dava-sobel',
-        attributes: {
-          title: 'Longitude',
-          author: 'Dava Sobel',
-        }
-      }, {
-        type: 'titles',
-        id: 'peter-the-great-robert-k-massie',
-        attributes: {
-          title: 'Peter The Great',
-          author: 'Robert K. Massie',
-        }
-      }, {
-        type: 'titles',
-        id: 'the-two-koreas-don-oberdorfer',
-        attributes: {
-          title: 'The Two Koreas',
-          author: 'Don Oberdorfer',
-        }
-      }]
-    };
+  this.get('/titles', (schema, request) => {
+    return schema.titles.all();
   });
 }
--- tests/acceptance/list-titles-test.js
+++ tests/acceptance/list-titles-test.js
@@ -12,8 +12,12 @@ test('visiting /titles', function(assert) {
 });
 
 test('should list books.', function (assert) {
+  let titles = server.createList('title', 3);
+
   visit('/titles');
+
   andThen(function () {
     assert.equal(find('.titles-entry').length, 3, 'should see 3 books');
+    assert.equal(find('.titles-entry:first').text(), titles[0].title);
   });
 });
