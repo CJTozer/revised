2c2194b80e427dd413a66ed44402a676162bb7bf
Chris Tozer <chris.tozer@metaswitch.com>
2016-11-28 00:33:04 +0000

    Add resources-list component with working materialized JS

---
+++ app/components/resources-list.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  didInsertElement: function () {
+    var collapsible = this.$('.collapsible').collapsible();
+    this.set('collapsible', collapsible);
+  },
+
+  willDestroyElement: function () {
+    this.get('collapsible').stop();
+  },
+});
