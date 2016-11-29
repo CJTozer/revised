c305221743fe41ba4b59802eca8d1034fc49410f
Chris Tozer <chris.tozer@metaswitch.com>
2016-11-29 00:21:35 +0000

    Fix up factory for resource data

--- mirage/factories/resource.js
+++ mirage/factories/resource.js
@@ -1,6 +1,28 @@
 import { Factory } from 'ember-cli-mirage';
 
 export default Factory.extend({
-  resourceType(i) { return `Text`; },
-  text(i) { return `Text for resource ${i}`; },
+  resourceType(i) {
+    switch(i % 3) {
+    case 0:
+      return "Text";
+    case 1:
+      return "Image";
+    case 2:
+      return "Link";
+    default:
+      return "Unknown";
+    }
+  },
+  text(i) {
+    switch(i % 3) {
+    case 0:
+      return "Some text for this resource";
+    case 1:
+      return `https://placekitten.com/${200+i}/${200+i}`;
+    case 2:
+      return "https://en.wikipedia.org/wiki/Special:Random";
+    default:
+      return "!!! Not implemented !!!";
+    }
+  },
 });
