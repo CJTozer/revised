6f0a94da6571b794f35ed512d852f02790d32c74
Chris Tozer <christopherjtozer@gmail.com>
2017-01-04 22:37:54 +0000

    Disable JShint for UT runs

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -11,7 +11,11 @@ module.exports = function(defaults) {
         'bower_components/materialize/sass'
       ],
       nodeSass: nodeSass // Workaround for ember-cli-sass bug https://github.com/aexmachina/ember-cli-sass/issues/117
-    }
+    },
+    // Turn off JShint, use ESlint instead.
+    'ember-cli-qunit': {
+      useLintTree: false,
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
