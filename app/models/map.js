9762ea158c03cd24b856a9bda4e97ec1026485c8
Chris Tozer <christopherjtozer@gmail.com>
2017-01-08 23:58:50 +0000

    Load map-maker using ember-browserify

--- app/models/map.js
+++ app/models/map.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import DS from 'ember-data';
-// import MapBuilder from 'map-maker';
+import MapBuilder from 'npm:map-maker';
 
 export default DS.Model.extend( {
   spec_obj: DS.attr(),
@@ -9,22 +9,21 @@ export default DS.Model.extend( {
   svg_data: Ember.computed( 'spec_obj', function() {
     var self = this;
     return DS.PromiseObject.create( {
-      // promise: new Promise( function( resolve, reject ) {
-      promise: new Promise( function( resolve ) {
-        // new MapBuilder()
-        //   .spec( this.get('specObj') )
-        //   .onError( ( err ) => {
-        //     reject( err );
-        //   } )
-        //   .onSuccess( ( data ) => {
-        //     self.set('retrieved_svg_data', data);
-        //     resolve( data );
-        //   } )
-        //   .build_map();
-        Ember.run.later(function() {
-          self.set('retrieved_svg_data', 'OKOKOK');
-          resolve( 'OK' );
-        }, 1000);
+      promise: new Promise( function( resolve, reject ) {
+        new MapBuilder()
+          .spec( this.get( 'specObj' ) )
+          .onError( ( err ) => {
+            reject( err );
+          } )
+          .onSuccess( ( data ) => {
+            self.set( 'retrieved_svg_data', data );
+            resolve( data );
+          } )
+          .build_map();
+        // Ember.run.later(function() {
+        //   self.set( 'retrieved_svg_data', 'OKOKOK' );
+        //   resolve( 'OK' );
+        // }, 1000);
       } ),
     } );
   } ),
